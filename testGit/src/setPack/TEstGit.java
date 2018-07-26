79d1909e7b582248014387de62b60e79e3f3c25e
Dell <Dell@192.168.1.6>
2018-07-26 10:21:29 +0530

    prem is commming ccode

---
+++ testGit/src/setPack/TEstGit.java
@@ -0,0 +1,15 @@
+package setPack;
+
+public class TEstGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcoem to git");
+		System.out.println("welcoem to git");
+		System.out.println("welcoem to git 26th ");
+		System.out.println("welcoem to git 26th ");
+		System.out.println("welcoem to git 26th ");
+		
+	}
+
+}
